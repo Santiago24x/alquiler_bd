8de2e09f6847e22536780a957776783e0b9b42b8
Santiago24x <santiagomarquez.it@gmail.com>
2024-11-29 15:12:15 -0500

    feat: :sparkles: Se agrega el proyecto base, estructurado para sprinboot

---
+++ AlquilerAutos/src/main/java/com/example/AlquilerAutos/AlquilerAutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlquilerAutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerAutosApplication.class, args);
+	}
+
+}
---
+++ AlquilerAutos/src/test/java/com/example/AlquilerAutos/AlquilerAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlquilerAutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
